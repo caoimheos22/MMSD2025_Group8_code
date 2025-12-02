c02b8530be04eb9584b4aeb4a7d8e8afbfe1d3f1
Caoimhe <caoimheosullivan22@gmail.com>
2025-12-02 11:19:09 +0000

    .

--- homepagefunctions.js
+++ homepagefunctions.js
@@ -3,6 +3,8 @@
 /*API*/
 /*https://api.openweathermap.org/data/2.5/weather?callback=process&lang=en&units=metric&q=Cork,Ireland&appid=9144a80c6727020a0a7106a1c6a63766  */
 
+
+
 /*Open and close sidebar*/
 function toggle_sidebar(){
     var sidebar = document.getElementById("sidebar");
