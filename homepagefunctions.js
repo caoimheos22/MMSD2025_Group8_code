75fc1d1896be6dcf24d2d6bf0892842db2f36f6c
Caoimhe <caoimheosullivan22@gmail.com>
2025-12-07 16:10:46 +0000

    get current coordinates

--- homepagefunctions.js
+++ homepagefunctions.js
@@ -1,50 +1,20 @@
 /*Group 8: Kyra O'Connor, Calum Cleary, Caoimhe O'Sullivan, Matthew Brink*/
 
-/*API*/
-/*https://api.openweathermap.org/data/2.5/weather?callback=process&lang=en&units=metric&q=Cork,Ireland&appid=9144a80c6727020a0a7106a1c6a63766  */
+/*get current coords*/
 
-
-
-/*Open and close sidebar*/
-function toggle_sidebar(){
-    var sidebar = document.getElementById("sidebar");
-
-
-    document.getElementById("sidebar").style.display = "block";
-    
+function getLocation(){
+    navigator.geolocation.getCurrentPosition (retrieveCoords);
 }
 
-/*get live location*/
-const result = document.querySelector("#live_location_result")
+function retrieveCoords(position){
+    var devLatitude = position.coords.latitude;
+    var devLongitude = position.coords.longitude;
 
-document.querySelector("#live_location").addEventListener (
-    "click", 
-    get_live_location
-);
-
-function get_live_location(){
-    if (!navigator.geolocation){
-        result.textContent = "Geolocation is not supported by your browser"
-        return;
-    }
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude; 
-        
-            result.textContent = `Latitude: ${lat}\nLongitude: ${lon}`;
-        },
-            
-    (err) => {
-        result.textContent = `Error: ${err.message}`;
-    }
-);
+    document.getElementById("current_coords").innerHTML = "lat: " + devLatitude + " long: " + devLongitude;
 }
 
+window.addEventListener("load", init);
 
-
-/*window.addEventListener("load", init);
-
-function init(
-
-)*/
+function init(){
+    document.getElementById("current_coords_but").addEventListener("click", getLocation)
+}
