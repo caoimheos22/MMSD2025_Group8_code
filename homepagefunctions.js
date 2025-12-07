bee2ded66a87747748718d1c53748183213682ba
Caoimhe <caoimheosullivan22@gmail.com>
2025-12-07 18:49:33 +0000

    get location - working

--- homepagefunctions.js
+++ homepagefunctions.js
@@ -1,20 +1,2 @@
 /*Group 8: Kyra O'Connor, Calum Cleary, Caoimhe O'Sullivan, Matthew Brink*/
 
-/*get current coords*/
-
-function getLocation(){
-    navigator.geolocation.getCurrentPosition (retrieveCoords);
-}
-
-function retrieveCoords(position){
-    var devLatitude = position.coords.latitude;
-    var devLongitude = position.coords.longitude;
-
-    document.getElementById("current_coords").innerHTML = "lat: " + devLatitude + " long: " + devLongitude;
-}
-
-window.addEventListener("load", init);
-
-function init(){
-    document.getElementById("current_coords_but").addEventListener("click", getLocation)
-}
